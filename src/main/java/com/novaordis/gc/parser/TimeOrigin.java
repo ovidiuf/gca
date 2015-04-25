32bac742edd852c15e9d42aecdca4af1a0714573
Ovidiu Feodorov <ovidiu@novaordis.com>
2015-04-25 02:57:22 -0700

    openjdk CMS log parses. 323/0/0.

---
+++ src/main/java/com/novaordis/gc/parser/TimeOrigin.java
@@ -0,0 +1,53 @@
+package com.novaordis.gc.parser;
+
+/**
+ * Static utility that converts "0.2210670 secs" into duration information (long milliseconds)
+ *
+ * @author <a href="mailto:ovidiu@novaordis.com">Ovidiu Feodorov</a>
+ *
+ *  Copyright 2013 Nova Ordis LLC
+ */
+public class TimeOrigin
+{
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    /**
+     * Static utility that converts "0.2210670 secs" into duration information (long milliseconds), performing all
+     * necessary rounding.
+     *
+     * @throws com.novaordis.gc.parser.ParserException
+     */
+    public static long toLongMilliseconds(String s, long lineNumber) throws ParserException
+    {
+        // we currently only handle 'secs', everything else is handled as a parsing error
+
+        if (!s.endsWith(" secs"))
+        {
+            throw new ParserException("we can only handle 'secs', but we got \"" + s + "\"", lineNumber);
+        }
+
+        s = s.substring(0, s.length() - " secs".length()).trim();
+        //noinspection UnnecessaryLocalVariable
+        long duration = Math.round(Float.parseFloat(s) * 1000);
+        return duration;
+    }
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+}
+
+
+
---
+++ src/test/java/com/novaordis/gc/parser/TimeOriginTest.java
@@ -0,0 +1,58 @@
+package com.novaordis.gc.parser;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:ovidiu@novaordis.com">Ovidiu Feodorov</a>
+ *
+ * Copyright 2013 Nova Ordis LLC
+ */
+public class TimeOriginTest extends Assert
+{
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    private static final Logger log = Logger.getLogger(TimeOriginTest.class);
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    @Test
+    public void testDuration() throws Exception
+    {
+        long ms = Duration.toLongMilliseconds("0.2210670 secs", -1L);
+        assertEquals(221L, ms);
+    }
+
+    @Test
+    public void weOnlyHandleSecs() throws Exception
+    {
+        try
+        {
+            Duration.toLongMilliseconds("0.2210670 years", 7L);
+            fail("should have failed because we only handle 'secs'");
+        }
+        catch(ParserException e)
+        {
+            log.info(e.getMessage());
+            assertEquals(7L, e.getLineNumber());
+        }
+    }
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+}
+
+
+
