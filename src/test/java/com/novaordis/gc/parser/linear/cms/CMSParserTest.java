0fd6e6565c83a21ae20b7c02598039b26472971e
Ovidiu Feodorov <ovidiu@novaordis.com>
2015-04-28 18:01:34 -0700

    started full cms cycle

--- src/test/java/com/novaordis/gc/parser/linear/cms/CMSParserTest.java
+++ src/test/java/com/novaordis/gc/parser/linear/cms/CMSParserTest.java
@@ -1,10 +1,19 @@
 package com.novaordis.gc.parser.linear.cms;
 
 import com.novaordis.gc.model.Timestamp;
+import com.novaordis.gc.model.event.GCEvent;
 import com.novaordis.gc.model.event.cms.CMSConcurrentMarkStart;
+import com.novaordis.gc.parser.GCLogParser;
+import com.novaordis.gc.parser.GCLogParserFactory;
+import com.novaordis.gc.parser.TimeOrigin;
+import com.novaordis.gc.parser.linear.LinearScanParser;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * @author <a href="mailto:ovidiu@novaordis.com">Ovidiu Feodorov</a>
@@ -43,6 +52,23 @@ public class CMSParserTest extends Assert
         assertEquals(0L, cms.getDuration());
 
         log.debug(".");
+    }
+
+    // cms full cycle --------------------------------------------------------------------------------------------------
+
+    @Test
+    public void cmsFullCycle() throws Exception
+    {
+        InputStream is = CMSParserTest.class.getClassLoader().getResourceAsStream("collected/cms-full-cycle.log");
+        assertNotNull(is);
+
+        InputStreamReader isr = new InputStreamReader(is);
+        GCLogParser p = GCLogParserFactory.getParser(isr);
+        assertTrue(p instanceof LinearScanParser);
+
+        List<GCEvent> events = p.parse(new TimeOrigin(0L));
+
+        fail("RETURN HERE");
     }
 
     // Package protected -----------------------------------------------------------------------------------------------
